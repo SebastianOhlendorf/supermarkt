92397d15de3f27fdb244cf5c1d3aeaa66ef63bf0
sebastian.ohlendorf <ohlendorf.s@gmail.com>
2021-02-01 20:01:07 +0100

    Erweiterung Klasse Ware
    - Die Superklasse Ware wurde angelegt und die ersten Methoden gebildet
    - Konstruktor erstellt
    - zwei Abstakte Methoden erstellt

---
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Ware.java
@@ -0,0 +1,51 @@
+package waren;
+
+import java.time.LocalDate;
+
+
+
+/**
+ * Klasse mit der neue Warenobjekte angelegt, Waren nachbestellt oder herausgegeben werden können.
+ * @author 
+ * @version
+ * @date
+ *
+ */
+public abstract class Ware {
+	
+	//Variablen anlegen
+	protected String name;
+	protected double preis;
+	protected int anzahl;
+	protected LocalDate seitWannImBestand;
+	
+	/**
+	 * Konstruktor der Klasse Ware um ein neues Warenobjekt zu erzeugen.
+	 * @author Sebastian Ohlendorf / 01.02.2020
+	 * @param name Die Bezeichnung der Ware oder des Produkts als String
+	 * @param preis Der Preis der Ware als Double
+	 */
+	public Ware(String name, double preis) {
+		
+		this.name = name;
+		this.preis = preis;
+		this.anzahl = 0;
+		this.seitWannImBestand = LocalDate.now();
+	}
+	
+	/**
+	 * Abstakte Methode zum nachbestellen von Waren.
+	 * Die Methode wird durch die Subklassen instanziert.
+	 * @param menge Gibt die Menge an, welche bestellt werden soll als Integer
+	 * @return Gibt ein True oder False zurück ob waren bestellt werden können
+	 */
+	public abstract boolean nachbestellen(int menge);
+	
+	/**
+	 * Abstakte Methode zum nachbestellen von Waren.
+	 * Die Methode wird durch die Subklassen instanziert.
+	 * @param menge Gibt die Menge an, welche herausgegeben werden kann als Integer
+	 * @return Gibt ein True oder False zurück ob Waren herausgegeben werden können
+	 */
+	public abstract boolean herausgeben(int menge);
+}
--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Waren.java
+++
@@ -1,5 +0,0 @@
-package waren;
-
-public class Waren {
-
-}

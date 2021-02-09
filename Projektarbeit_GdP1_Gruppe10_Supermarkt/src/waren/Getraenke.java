fc1d72680b097362492d8d277db75bd13388ad16
lennart <lennart@DESKTOP-17PT6JE.speedport.ip>
2021-02-09 18:06:40 +0100

    Getranke bearbeitet

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Getraenke.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Getraenke.java
@@ -4,13 +4,16 @@ import java.time.LocalDate;
 
 /**
  * Kinderklasse der Klasse Ware zum anlegen von Getränke-Objekten
- * @author 
- * @version
- * @date
+ * @author Lennart Sparbier
+ * @version 1.0
+ * @date 09.02.2021
  *
  */
 public class Getraenke extends Ware {
 
+	protected double alcProzente;
+	
+	
 	/**
 	 * 
 	 * @param name
@@ -18,10 +21,29 @@ public class Getraenke extends Ware {
 	 * @param seitWannImBestand
 	 * @param anzahl
 	 */
-	public Getraenke(String name, double preis, LocalDate seitWannImBestand, int anzahl) {
+	public Getraenke(String name, double preis, LocalDate seitWannImBestand, int anzahl, 
+			boolean obImBestand, int alcProzente)  {
 		super(name, preis, anzahl, seitWannImBestand);
 		// TODO Auto-generated constructor stub
+		
+		this.alcProzente = alcProzente;
+		
+		// Prüfung ob bei Anlegung Getränk bereits 30 verschiedene Getränke vorhanden sind
+		int anzahlGetraenke = 0;
+		this.anzahl = 0;
+		
+		if(anzahlGetraenke <=30) {
+			
+			this.anzahl = 100;
+			this.seitWannImBestand = LocalDate.now();
+		} else {
+			System.out.println("Es sind bereits 30 Getränke im System vermerkt. Es ist nicht möglich weitere Getränke anzulegen.");
+		}
+		
+		
 	}
+	
+	
 
 	@Override
 	public boolean nachbestellen(int menge) {
@@ -40,8 +62,15 @@ public class Getraenke extends Ware {
 		return null;
 	}
 	
-	public boolean istAlkoholhaltig() {
-		return false;
+	public boolean istAlkoholhaltig(boolean alkoholhaltig) {
+		
+		if ( this.alcProzente > 0) {
+			alkoholhaltig = true;
+		} else {
+			System.out.println("Diese Getränk enthält keinen Alkohol");
+		}
+		
+		return alkoholhaltig;
 	}
 	
 	public static void gebeGetraenkeAus() {

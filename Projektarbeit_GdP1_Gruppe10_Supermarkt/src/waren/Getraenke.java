b3886e157abb20a5ec6179cb38da7be329e08969
lennart <lennart@DESKTOP-17PT6JE.speedport.ip>
2021-02-11 01:42:01 +0100

    wird langsam -> getränke

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Getraenke.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Getraenke.java
@@ -1,6 +1,6 @@
 package waren;
-
 import java.time.LocalDate;
+import java.util.Scanner;
 
 /**
  * Kinderklasse der Klasse Ware zum anlegen von Getränke-Objekten
@@ -26,21 +26,56 @@ public class Getraenke extends Ware {
 		super(name, preis, anzahl, seitWannImBestand);
 		// TODO Auto-generated constructor stub
 		
+		Scanner in = new Scanner(System.in);
+		
 		this.alcProzente = alcProzente;
 		
 		// Prüfung ob bei Anlegung Getränk bereits 30 verschiedene Getränke vorhanden sind
-		int anzahlGetraenke = 0;
+		int getraenkeInsgesamt = 0;
 		this.anzahl = 0;
+		boolean enthaeltAlc = false;
 		
-		if(anzahlGetraenke <=30) {
+		String [] getraenkeArray;
+		getraenkeArray = new String [30];
+		
+		if(getraenkeInsgesamt <=30) {
+			
+		for (int i = 0; i <= getraenkeArray.length; i++) {
+			
+			System.out.println("Bitte geben Sie einen Namen für das Getränk ein ");
+			getraenkeArray[i] = in.next();
+			
+			System.out.println("Bitte geben Sie einen Preis für das Getränk ein");
+			getraenkeArray[i] = in.nextDouble();
+			
+			System.out.println("Bitte geben Sie einen Alkoholgehalt an");
+			this.alcProzente = in.nextDouble();
+			istAlkoholhaltig(enthaeltAlc);
+			
+			
 			
 			this.anzahl = 100;
 			this.seitWannImBestand = LocalDate.now();
+			//this.ObImBestand = true;
+			
+
+			i++;
+			getraenkeInsgesamt ++;
+			
+		} 
+		
 		} else {
-			System.out.println("Es sind bereits 30 Getränke im System vermerkt. Es ist nicht möglich weitere Getränke anzulegen.");
+			System.out.println("Es wurden bereits 30 Getraenke erstellt");
+			
+			in.close();
 		}
 		
+		// Besprechen: Müsste mit in Ware als Vererbung	obImBestand = true;!!
+		//this.obImBestand = true; ( Benötigen im Hauptprogramm if abfrage 
+		// if this.anzahl = 0  this.obImBestand= false)
 		
+		// Die Ware soll ins Lager gepackt werden Klasse Lager
+			
 	}
 	
 	
@@ -64,10 +99,11 @@ public class Getraenke extends Ware {
 	
 	public boolean istAlkoholhaltig(boolean alkoholhaltig) {
 		
+		
 		if ( this.alcProzente > 0) {
 			alkoholhaltig = true;
 		} else {
-			System.out.println("Diese Getränk enthält keinen Alkohol");
+			System.out.println("Dieses Getränk enthält keinen Alkohol");
 		}
 		
 		return alkoholhaltig;
@@ -75,10 +111,16 @@ public class Getraenke extends Ware {
 	
 	public static void gebeGetraenkeAus() {
 		
+		
+		//Gibt eine Liste aller vorhandener Lebensmittel (ihrer Namen) nummeriert aus. (Dies soll dem Nutzer später als Auswahl dienen, zB:
+		//	(1) Schokolade
+		//	(2) Nudeln
+		//	(3) Gurke
+		//	… )
 	}
 	
 	public static void gebeNonAlkGetraenkeAus() {
-		
+		//für nicht alkoholische Getränke
 	}
 
 }

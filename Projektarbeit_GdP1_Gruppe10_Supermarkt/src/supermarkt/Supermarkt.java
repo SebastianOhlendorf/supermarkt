dbabbc7cf6ee89904de23f05229c400dbd310fbf
ethan <ethan.stapff@gmail.com>
2021-02-13 17:59:35 +0100

    EinAusgabe/ Supermarkt

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/supermarkt/Supermarkt.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/supermarkt/Supermarkt.java
@@ -1,10 +1,36 @@
 package supermarkt;
 
+
 public class Supermarkt {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+      
+		begruessung();
+
+
+		switch (auswahl()) {
+			case 1: break;
+
+			case 2: break;
+		}
 
 	}
+	public static void begruessung() {
 
+		System.out.println("text");
+	}
+	public static void auswahlmenu () {
+	
+		System.out.println("Auswahlmenu");
+	}
+	public static void auswahl() {
+
+	}
+	public static void schliessung () {
+
+		//close;
+	}
 }

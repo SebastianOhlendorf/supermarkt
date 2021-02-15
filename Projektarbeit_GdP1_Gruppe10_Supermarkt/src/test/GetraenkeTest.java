08ab9a52e77173ab1854f810c6a54f3d4f4c6eed
lennart <lennart@DESKTOP-17PT6JE.speedport.ip>
2021-02-16 00:45:07 +0100

    probieren

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/test/GetraenkeTest.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/test/GetraenkeTest.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 
 import supermarkt.SupermarktExceptions;
 import waren.Getraenke;
-
+//jojo
 public class GetraenkeTest {
 
 	public static void main(String[] args) throws SupermarktExceptions {

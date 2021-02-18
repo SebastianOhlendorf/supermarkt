ffd6b9c0b35276f915b8e088e79eb806bfacc075
lennart <lennart@DESKTOP-17PT6JE.speedport.ip>
2021-02-18 17:48:21 +0100

    filme

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Filme.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/waren/Filme.java
@@ -1,5 +1,47 @@
 package waren;
+import java.time.LocalDate;
 
-public class Filme {
+/**
+ * Kinderklasse der Klasse NonFoodArtikel zum anlegen von Film-Objekten
+ * @author Lennart Sparbier
+ * @version 1.0
+ * @date 18.02.2021
+ *
+ */
+public class Filme extends Medien {
 
+	
+		//Objektattribute speziell für die Filme
+		protected double dauer;
+		protected int FSK;
+		protected String sprache;
+		
+		//Klassenattribute
+		private static int zaehler_medien = 0;
+		
+		public Filme(
+				 String name, double preis, LocalDate seitWannImBestand, int anzahl, 
+				 String beschreibung, int unterGruppe, boolean isDigital, String genre, int erscheinungsjahr,
+				 double dauer, int FSK, String sprache) {
+		super( name,  preis,  seitWannImBestand,  anzahl, 
+			  beschreibung,  unterGruppe,  isDigital,  genre, erscheinungsjahr);
+			
+			this.dauer = dauer;
+			this.FSK = FSK;
+			this.sprache = sprache;
+			// TODO Auto-generated constructor stub
+		}
+		
+		public static void gibFilmeAus() {
+			
 }
+
+		@Override
+		// toString Methode um alles Daten leserlich als String auszugeben
+		public String toString() {
+		return "neuer Film Artikel [ name= "+ name + " preis= "+ preis + " seitWannImBestand= " + seitWannImBestand +  
+		" anzahl= " + anzahl +  " beschreibung= " + beschreibung + " unterGruppe = "+ unterGruppe + 
+		" isDigital= " + isDigital + " genre= " + genre + " erscheinungsjahr= " + erscheinungsjahr + 
+		" dauer= " + dauer + " FSK= " + FSK + " sprache= " + sprache + "]";
+}
+}	

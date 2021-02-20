ba4e2097b911fb545fc79b86c7e819fed42d6a59
sebastian.ohlendorf <ohlendorf.s@gmail.com>
2021-02-20 21:26:19 +0100

    Nonfood Artikel nach der neuen Arraylogik angepasst

--- Projektarbeit_GdP1_Gruppe10_Supermarkt/src/test/NonFoodArtikelTestMain.java
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/test/NonFoodArtikelTestMain.java
@@ -1,52 +1,38 @@
 package test;
 import java.time.LocalDate;
-import java.util.Scanner;
-
-import supermarkt.einAusgabe;
 import waren.NonFoodArtikel;
 public class NonFoodArtikelTestMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		
-		/**
-		 * Deklaration von Variablen 
+		/*
+		 * protected static final int KLEIDUNG = 5;
+		 * protected static final int MEDIEN = 6; 
+		 * protected static final int DROGERIEARTIKEL = 7;
+		 * protected static final int FILME = 8; 
+		 * 
+		 * String name, double preis, LocalDate seitWannImBestand, int anzahl, 
+			 String beschreibung, int unterGruppe
 		 * 
 		 */
-		String name;
-		double preis;
-		int anzahl;
-		LocalDate seitWannImBestand = LocalDate.now();
-		String obImBestand = "";
 		
-		//speziell für NonFoodArtikel
-		String beschreibung;
-		String unterGruppe;
-		 
-		/**
-		 * Eingaben vom Benutzer
-		 */
+		NonFoodArtikel nonfood = new NonFoodArtikel("Jacke" + 0, 2.98, LocalDate.now(), 0, "Jacke zum anziehen", 5);
+		NonFoodArtikel.addNonFoodArtikel(nonfood);
+		nonfood.nachbestellen(5);
+		nonfood.herausgeben(5);
+		nonfood.nachbestellen(200);
+		nonfood.herausgeben(200);
+		NonFoodArtikel lebensmittel2 = new NonFoodArtikel("Musik" + 0, 2.98, LocalDate.now(), 0, "Musik CD", 6);
+		NonFoodArtikel.addNonFoodArtikel(lebensmittel2);
+						
+		for(int i = 0; i < 40; i++) {
+			NonFoodArtikel lebensmittel5 = new NonFoodArtikel("Shampoo" + i, 2.98, LocalDate.now(), 0, "Zum waschen von Haaren", 7);
+			NonFoodArtikel.addNonFoodArtikel(lebensmittel5);
+			
+		}
+
+		NonFoodArtikel.gebenNFArtikelAus();
 		
-		name= einAusgabe.eingabeName("Bitte geben Sie einen Namen für das Non Food Produkt ein: ");
-		preis= einAusgabe.eingabePreis("Bitte geben Sie einen Preis für den Non Food Artikel ein: ");
-		anzahl= einAusgabe.eingabeAnzahl("Die Anzahl im Lager beträgt 100");
-		seitWannImBestand= einAusgabe.eingabeSeitWannImBestand("Der Artikel ist seit " + seitWannImBestand + " im Bestand");
-		
-		
-		//speziell für NonFoodArtikel
-		beschreibung= einAusgabe.eingabeBeschreibung("Bitte geben Sie eine ausführliche Beschreibung zu dem Non Food Produkt ein");
-		unterGruppe = einAusgabe.eingabeUnterGruppe("zu welcher Untergruppe gehört das Produkt?");
-		
-		
-		/**
-		 * Erzeugung eines neuen NonFood Artikels   
-		 */
-		
-		NonFoodArtikel neueErstellung = new NonFoodArtikel(name,preis,seitWannImBestand,anzahl,
-				obImBestand,beschreibung,unterGruppe);
-		
-		System.out.println("der neue NonFoodArtikel im Überblick" + neueErstellung);
 	}
 
 }

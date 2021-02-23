9eca717bd2f03fd8f22eda64490f7ab6c1a42b55
sebastian.ohlendorf <ohlendorf.s@gmail.com>
2021-02-23 22:22:17 +0100

    Neue Ausgabenklassen

---
+++ Projektarbeit_GdP1_Gruppe10_Supermarkt/src/hilfsklassen/Ausgaben.java
@@ -0,0 +1,240 @@
+package hilfsklassen;
+
+/**
+ * Diese Klasse dient als Hilfsklasse und ist eine Sammlung verschiedener Ausgaben die in der Verarbeitung des Programms verwendet werden. 
+ * @author Sebastian Ohlendorf
+ * @version 1.0
+ * @date 23.02.20201
+ *
+ */
+public class Ausgaben {
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Supermarktlogos
+	 */
+	public static void supermaktLogo() {
+		
+		System.out.print(
+				    "###################################################################################################################\r\n"
+				  + "#                                                                                                                 #\r\n"
+				  + "#  .oooooo..o                                                                                oooo            .    #\r\n"
+				  + "# d8P'    `Y8                                                                                `888          .o8    #\r\n"
+				  + "# Y88bo.      oooo  oooo  oo.ooooo.   .ooooo.  oooo d8b ooo. .oo.  .oo.    .oooo.   oooo d8b  888  oooo  .o888oo  #\r\n"
+				  + "#  `\"Y8888o.  `888  `888   888' `88b d88' `88b `888\"\"8P `888P\"Y88bP\"Y88b  `P  )88b  `888\"\"8P  888 .8P'     888    #\r\n"
+				  + "#      `\"Y88b  888   888   888   888 888ooo888  888      888   888   888   .oP\"888   888      888888.      888    #\r\n"
+				  + "# oo     .d8P  888   888   888   888 888    .o  888      888   888   888  d8(  888   888      888 `88b.    888 .  #\r\n"
+				  + "# 8\"\"88888P'   `V88V\"V8P'  888bod8P' `Y8bod8P' d888b    o888o o888o o888o `Y888\"\"8o d888b    o888o o888o   \"888\"  #\r\n"
+				  + "#                          888                                                                                    #\r\n"
+				  + "#                         o888o                                                                                   #\r\n"
+				  + "#                                                                                                                 #\r\n"
+				  + "###################################################################################################################\r\n");
+                                     
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Gruppe 10
+	 */
+	public static void gruppeZehn() {
+		
+		System.out.print(
+				  "###################################################################################################################\r\n"
+				+ "#   .oooooo.                                                                .o    .oooo.                          #\r\n"
+				+ "#  d8P'  `Y8b                                                             o888   d8P'`Y8b                         #\r\n"
+				+ "# 888           oooo d8b oooo  oooo  oo.ooooo.  oo.ooooo.   .ooooo.        888  888    888                        #\r\n"
+				+ "# 888           `888\"\"8P `888  `888   888' `88b  888' `88b d88' `88b       888  888    888                        #\r\n"
+				+ "# 888     ooooo  888      888   888   888   888  888   888 888ooo888       888  888    888                        #\r\n"
+				+ "# `88.    .88'   888      888   888   888   888  888   888 888    .o       888  `88b  d88'                        #\r\n"
+				+ "#  `Y8bood8P'   d888b     `V88V\"V8P'  888bod8P'  888bod8P' `Y8bod8P'      o888o  `Y8bd8P'                         #\r\n"
+				+ "#                                     888        888                                                              #\r\n"
+				+ "#                                    o888o      o888o                                                             #\r\n"
+				+ "###################################################################################################################\r\n");
+		
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Hauptmenü
+	 */
+	public static void hauptmenue() {
+		
+		System.out.print(
+				  "###################################################################################################################\r\n"
+				+ "# ooooo   ooooo                                      .                                            o8o   o8o       #\r\n"
+				+ "# `888'   `888'                                    .o8                                            `\"'   `\"'       #\r\n"
+				+ "#  888     888   .oooo.   oooo  oooo  oo.ooooo.  .o888oo ooo. .oo.  .oo.    .ooooo.  ooo. .oo.   oooo  oooo       #\r\n"
+				+ "#  888ooooo888  `P  )88b  `888  `888   888' `88b   888   `888P\"Y88bP\"Y88b  d88' `88b `888P\"Y88b  `888  `888       #\r\n"
+				+ "#  888     888   .oP\"888   888   888   888   888   888    888   888   888  888ooo888  888   888   888   888       #\r\n"
+				+ "#  888     888  d8(  888   888   888   888   888   888 .  888   888   888  888    .o  888   888   888   888       #\r\n"
+				+ "# o888o   o888o `Y888\"\"8o  `V88V\"V8P'  888bod8P'   \"888\" o888o o888o o888o `Y8bod8P' o888o o888o  `V88V\"V8P'      #\r\n"
+				+ "#                                      888                                                                        #\r\n"
+				+ "#                                     o888o                                                                       #\r\n"
+				+ "###################################################################################################################\r\n");
+		
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Lebensmittel
+	 */
+	public static void untermenueLebensmittel() {
+		
+		System.out.print(
+				  "############################################################################################################################\r\n"
+				+ "# ooooo                   .o8                                                         o8o      .       .             oooo  #\r\n"
+				+ "# `888'                  \"888                                                         `\"'    .o8     .o8             `888  #\r\n"
+				+ "#  888          .ooooo.   888oooo.   .ooooo.  ooo. .oo.    .oooo.o ooo. .oo.  .oo.   oooo  .o888oo .o888oo  .ooooo.   888  #\r\n"
+				+ "#  888         d88' `88b  d88' `88b d88' `88b `888P\"Y88b  d88(  \"8 `888P\"Y88bP\"Y88b  `888    888     888   d88' `88b  888  #\r\n"
+				+ "#  888         888ooo888  888   888 888ooo888  888   888  `\"Y88b.   888   888   888   888    888     888   888ooo888  888  #\r\n"
+				+ "#  888       o 888    .o  888   888 888    .o  888   888  o.  )88b  888   888   888   888    888 .   888 . 888    .o  888  #\r\n"
+				+ "# o888ooooood8 `Y8bod8P'  `Y8bod8P' `Y8bod8P' o888o o888o 8\"\"888P' o888o o888o o888o o888o   \"888\"   \"888\" `Y8bod8P' o888o #\r\n"
+				+ "#                                                                                                                          #\r\n"
+				+ "############################################################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Backware auf der Konsole
+	 */
+	public static void untermenueBackwaren() {
+		
+		System.out.print(
+				  "###########################################################################################################\r\n"
+				+ "# oooooooooo.                      oooo                                                                   #\r\n"
+				+ "# `888'   `Y8b                     `888                                                                   #\r\n"
+				+ "#  888     888  .oooo.    .ooooo.   888  oooo  oooo oooo    ooo  .oooo.   oooo d8b  .ooooo.  ooo. .oo.    #\r\n"
+				+ "#  888oooo888' `P  )88b  d88' `\"Y8  888 .8P'    `88. `88.  .8'  `P  )88b  `888\"\"8P d88' `88b `888P\"Y88b   #\r\n"
+				+ "#  888    `88b  .oP\"888  888        888888.      `88..]88..8'    .oP\"888   888     888ooo888  888   888   #\r\n"
+				+ "#  888    .88P d8(  888  888   .o8  888 `88b.     `888'`888'    d8(  888   888     888    .o  888   888   #\r\n"
+				+ "# o888bood8P'  `Y888\"\"8o `Y8bod8P' o888o o888o     `8'  `8'     `Y888\"\"8o d888b    `Y8bod8P' o888o o888o  #\r\n"
+				+ "#                                                                                                         #\r\n"
+				+ "###########################################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Getränke auf der Konsole
+	 */
+	public static void untermenueGetraenke() {
+		
+		System.out.print(
+				  "##########################################################################################\r\n"
+				+ "#   .oooooo.                  .            o8o   o8o             oooo                    #\r\n"
+				+ "#  d8P'  `Y8b               .o8            `\"'   `\"'             `888                    #\r\n"
+				+ "# 888            .ooooo.  .o888oo oooo d8b  .oooo.   ooo. .oo.    888  oooo   .ooooo.    #\r\n"
+				+ "# 888           d88' `88b   888   `888\"\"8P `P  )88b  `888P\"Y88b   888 .8P'   d88' `88b   #\r\n"
+				+ "# 888     ooooo 888ooo888   888    888      .oP\"888   888   888   888888.    888ooo888   #\r\n"
+				+ "# `88.    .88'  888    .o   888 .  888     d8(  888   888   888   888 `88b.  888    .o   #\r\n"
+				+ "#  `Y8bood8P'   `Y8bod8P'   \"888\" d888b    `Y888\"\"8o o888o o888o o888o o888o `Y8bod8P'   #\r\n"
+				+ "#                                                                                        #\r\n"
+				+ "##########################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels NonFood auf der Konsole
+	 */
+	public static void untermenueNonFood() {
+		
+		System.out.print(
+				  "####################################################################################\r\n"
+				+ "# ooooo      ooo                       oooooooooooo                           .o8  #\r\n"
+				+ "# `888b.     `8'                       `888'     `8                          \"888  #\r\n"
+				+ "#  8 `88b.    8   .ooooo.  ooo. .oo.    888          .ooooo.   .ooooo.   .oooo888  #\r\n"
+				+ "#  8   `88b.  8  d88' `88b `888P\"Y88b   888oooo8    d88' `88b d88' `88b d88' `888  #\r\n"
+				+ "#  8     `88b.8  888   888  888   888   888    \"    888   888 888   888 888   888  #\r\n"
+				+ "#  8       `888  888   888  888   888   888         888   888 888   888 888   888  #\r\n"
+				+ "# o8o        `8  `Y8bod8P' o888o o888o o888o        `Y8bod8P' `Y8bod8P' `Y8bod88P\" #\r\n"
+				+ "#                                                                                  #\r\n"
+				+ "####################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Medien auf der Konsole
+	 */
+	public static void untermenueMedien() {
+		
+		System.out.print(
+				  "######################################################################\r\n"
+				+ "# ooo        ooooo                 .o8   o8o                         #\r\n"
+				+ "# `88.       .888'                \"888   `\"'                         #\r\n"
+				+ "#  888b     d'888   .ooooo.   .oooo888  oooo   .ooooo.  ooo. .oo.    #\r\n"
+				+ "#  8 Y88. .P  888  d88' `88b d88' `888  `888  d88' `88b `888P\"Y88b   #\r\n"
+				+ "#  8  `888'   888  888ooo888 888   888   888  888ooo888  888   888   #\r\n"
+				+ "#  8    Y     888  888    .o 888   888   888  888    .o  888   888   #\r\n"
+				+ "# o8o        o888o `Y8bod8P' `Y8bod88P\" o888o `Y8bod8P' o888o o888o  #\r\n"
+				+ "#                                                                    #\r\n"
+				+ "######################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Kleidung auf der Konsole
+	 */
+	public static void untermenueKleidung() {
+		
+		System.out.print(
+				  "#####################################################################################\r\n"
+				+ "# oooo    oooo oooo             o8o        .o8                                      #\r\n"
+				+ "# `888   .8P'  `888             `\"'       \"888                                      #\r\n"
+				+ "#  888  d8'     888   .ooooo.  oooo   .oooo888  oooo  oooo  ooo. .oo.    .oooooooo  #\r\n"
+				+ "#  88888[       888  d88' `88b `888  d88' `888  `888  `888  `888P\"Y88b  888' `88b   #\r\n"
+				+ "#  888`88b.     888  888ooo888  888  888   888   888   888   888   888  888   888   #\r\n"
+				+ "#  888  `88b.   888  888    .o  888  888   888   888   888   888   888  `88bod8P'   #\r\n"
+				+ "# o888o  o888o o888o `Y8bod8P' o888o `Y8bod88P\"  `V88V\"V8P' o888o o888o `8oooooo.   #\r\n"
+				+ "#                                                                       d\"     YD   #\r\n"
+				+ "#                                                                       \"Y88888P'   #\r\n"
+				+ "#####################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Drogerie auf der Konsole
+	 */
+	public static void untermenueDrogerieArtikel() {
+		
+		System.out.print(
+				  "###################################################################################\r\n"
+				+ "# oooooooooo.                                                     o8o             #\r\n"
+				+ "# `888'   `Y8b                                                    `\"'             #\r\n"
+				+ "#  888      888 oooo d8b  .ooooo.   .oooooooo  .ooooo.  oooo d8b oooo   .ooooo.   #\r\n"
+				+ "#  888      888 `888\"\"8P d88' `88b 888' `88b  d88' `88b `888\"\"8P `888  d88' `88b  #\r\n"
+				+ "#  888      888  888     888   888 888   888  888ooo888  888      888  888ooo888  #\r\n"
+				+ "#  888     d88'  888     888   888 `88bod8P'  888    .o  888      888  888    .o  #\r\n"
+				+ "# o888bood8P'   d888b    `Y8bod8P' `8oooooo.  `Y8bod8P' d888b    o888o `Y8bod8P'  #\r\n"
+				+ "#                                  d\"     YD                                      #\r\n"
+				+ "#                                  \"Y88888P'                                      #\r\n"
+				+ "###################################################################################\r\n");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe des Titels Filme auf der Konsole
+	 */
+	public static void untermenueFilme() {
+		
+		System.out.print(
+				  "#########################################################\r\n"
+				+ "# oooooooooooo  o8o  oooo                               #\r\n"
+				+ "# `888'     `8  `\"'  `888                               #\r\n"
+				+ "#  888         oooo   888  ooo. .oo.  .oo.    .ooooo.   #\r\n"
+				+ "#  888oooo8    `888   888  `888P\"Y88bP\"Y88b  d88' `88b  #\r\n"
+				+ "#  888    \"     888   888   888   888   888  888ooo888  #\r\n"
+				+ "#  888          888   888   888   888   888  888    .o  #\r\n"
+				+ "# o888o        o888o o888o o888o o888o o888o `Y8bod8P'  #\r\n"
+				+ "#                                                       #\r\n"
+				+ "#########################################################");
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe Begrüßung auf der Konsole
+	 */
+	public static void begruessung() {
+		
+		System.out.println("Willkommen im Supermakt Programm der Gruppe 10.\n"
+				+ "Wir freuen und das Sie sich für unser Programm entschieden haben.\n");
+		
+	}
+	
+	/**
+	 * Klassenmethode zur Ausgabe Verabschiedung auf der Konsole
+	 */
+	public static void beendigung() {
+		
+		System.out.println("Vielen Dank das sie das Supermakt Programm der Gruppe 10 genutzt haben.\n"
+				+ "Wir wünschen noch einen schönen Tag.\n");
+	}
+	
+
+}
